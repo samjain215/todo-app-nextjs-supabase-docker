1cb9ad7445aff0e6c29575a0426302a1517cfb7f
Samyak Jain <samyak@Samyaks-MBP.lan>
2024-11-25 21:42:25 -0800

    framework created and login with supabase completed

---
+++ project310_todo/app/home/page.tsx
@@ -0,0 +1,94 @@
+"use client"
+
+import { supabase } from "@/lib/supabase";
+import { useEffect, useState } from "react"
+
+type Task = {
+    "task_id": number,
+    "user_id": number,
+    "title": string,
+    "description": string,
+    "priority_id": number,
+    "completed": boolean
+    "due_date": string
+}
+
+export default function Home() {
+    const [userId, setUserId] = useState("");
+    const [tasks, setTasks] = useState < Task[] > ([]);
+    const getUser = async () => {
+        const data = await supabase.auth.getUser();
+        console.log("DATA: ", data);
+        setUserId(data.data.user?.id ?? "123");
+    };
+
+    const handleToggleTask = (taskId: number) => {
+        const updatedTasks = tasks.map(task =>
+            task.task_id === taskId ? { ...task, completed: !task.completed } : task
+        );
+        setTasks(sortTasks(updatedTasks));
+    };
+
+    const sortTasks = (tasks: Task[]): Task[] => {
+        return tasks.sort((a, b) => Number(a.completed) - Number(b.completed));
+    };
+
+    useEffect(() => {
+        getUser();
+        fetch("/api/tasks")
+            .then((response) => response.json())
+            .then((json) => {
+                if (json['tasks'].length > 0)
+                    setTasks(sortTasks(json['tasks']));
+            });
+    }, []);
+    return (
+        <div className="w-full h-screen flex flex-col bg-gray bg-cover bg-center bg-[url('/background.png')]">
+            <div className="grid grid-cols-2 grid-rows-2 gap-4 h-full p-4">
+                <div className="bg-white rounded-lg text-black shadow-md">
+                    <div className="flex justify-between items-center mt-1 mb-2">
+                        <div className="flex ml-2">
+                            <div className="h-5 w-5 text-sm border rounded-full bg-red-500 text-white mr-2 flex items-center justify-center">
+                                Ⅰ
+                            </div>
+                            <p className="text-red-500 text-sm">Urgent & Important</p>
+                        </div>
+                        <button className="text-2xl mr-3">+</button>
+                    </div>
+                    <ul className="mr-4 ml-2">
+                        {tasks.map((task: Task) => (
+                            <div key={task.task_id} className="mt-2 border-b border-red-200 text-sm">
+                                <li key={task.task_id} className="flex items-center justify-between mb-1">
+                                    <div>
+                                        <input
+                                            type="checkbox"
+                                            className="mr-2 w-3 h-3"
+                                            checked={task.completed}
+                                            color="#EF4444"
+                                            onChange={() => handleToggleTask(task.task_id)}
+                                        />
+                                        <span className={`${task.completed ? 'line-through text-gray-500' : ''}`}>
+                                            {task.title}
+                                        </span>
+                                    </div>
+                                    <div>
+                                        {task.due_date}
+                                    </div>
+                                </li>
+                            </div>
+                        ))}
+                    </ul>
+                </div>
+                <div className="bg-white rounded-lg text-black flex items-center justify-center shadow-md">
+                    Card 2
+                </div>
+                <div className="bg-white rounded-lg text-black flex items-center justify-center shadow-md">
+                    Card 3
+                </div>
+                <div className="bg-white rounded-lg text-black flex items-center justify-center shadow-md">
+                    Card 4
+                </div>
+            </div>
+        </div>
+    );
+}
