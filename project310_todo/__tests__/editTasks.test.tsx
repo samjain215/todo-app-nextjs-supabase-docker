2d823aa47185d5869d3b4e0f956e5060118c0f29
Samyak Jain <samyak@Samyaks-MacBook-Pro.local>
2024-12-03 14:44:51 -0800

    edit task implementation in progress

--- project310_todo/__tests__/editTasks.test.tsx
+++ project310_todo/__tests__/editTasks.test.tsx
@@ -33,14 +33,55 @@ global.fetch = jest.fn(() =>
 );
 
 const USER = {
-  data: {
-    user: {
-      id: 'c3978581-4ec1-4f63-9cb1-f2583d2f4b73',
-      // ... other user data
-    },
+  "data": {
+    "user": {
+      "id": "c3978581-4ec1-4f63-9cb1-f2583d2f4b73",
+      "aud": "authenticated",
+      "role": "authenticated",
+      "email": "sam.jain2122@gmail.com",
+      "email_confirmed_at": "2024-11-26T02:01:04.576175Z",
+      "phone": "",
+      "confirmation_sent_at": "2024-11-26T02:00:38.992196Z",
+      "confirmed_at": "2024-11-26T02:01:04.576175Z",
+      "recovery_sent_at": "2024-12-01T23:36:57.621578Z",
+      "last_sign_in_at": "2024-11-26T02:01:04.952076Z",
+      "app_metadata": {
+        "provider": "email",
+        "providers": [
+          "email"
+        ]
+      },
+      "user_metadata": {
+        "email": "sam.jain2122@gmail.com",
+        "email_verified": false,
+        "phone_verified": false,
+        "sub": "c3978581-4ec1-4f63-9cb1-f2583d2f4b73"
+      },
+      "identities": [
+        {
+          "identity_id": "56a7e5ea-b2f9-4764-ab07-a1326d61bc17",
+          "id": "c3978581-4ec1-4f63-9cb1-f2583d2f4b73",
+          "user_id": "c3978581-4ec1-4f63-9cb1-f2583d2f4b73",
+          "identity_data": {
+            "email": "sam.jain2122@gmail.com",
+            "email_verified": false,
+            "phone_verified": false,
+            "sub": "c3978581-4ec1-4f63-9cb1-f2583d2f4b73"
+          },
+          "provider": "email",
+          "last_sign_in_at": "2024-11-26T02:00:38.986621Z",
+          "created_at": "2024-11-26T02:00:38.986695Z",
+          "updated_at": "2024-11-26T02:00:38.986695Z",
+          "email": "sam.jain2122@gmail.com"
+        }
+      ],
+      "created_at": "2024-11-26T02:00:38.98197Z",
+      "updated_at": "2024-12-02T03:36:59.36258Z",
+      "is_anonymous": false
+    }
   },
-  error: null,
-};
+  "error": null
+}
 
 jest.mock('@supabase/auth-helpers-nextjs', () => {
   return {
@@ -67,7 +108,7 @@ describe('NewHome Component', () => {
     expect(screen.getByText(/Eisenhower Matrix/i)).toBeInTheDocument();
 
     // Verify the initial task is present
-    expect(screen.getByText('Let's Go to Italy')).toBeInTheDocument();
+    expect(screen.getByText("Let's Go to Italy")).toBeInTheDocument();
 
     // Find the edit button for the task with task_id '1'
     // Assuming it has data-testid='edit-button-1'
